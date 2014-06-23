85e9e2384678619c80dc9c6376d06923b2dfb0f1
Martin Autumn <martin@herbst-winter.net>
2014-06-23 12:58:11 +0200

    Read xml data set and display images on test page

---
+++ src/DataSet.js
@@ -0,0 +1,16 @@
+function DataSet(path) {
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.open("GET", path, false);
+	xhttp.send();
+	var xmlDoc = xhttp.responseXML;
+
+	// var oSerializer = new XMLSerializer();
+	// var sXML = oSerializer.serializeToString(xmlDoc);
+	// console.log(xmlDoc);
+
+	var x2js = new X2JS();
+
+	var json = x2js.xml2json( xmlDoc );
+	return json;
+}
